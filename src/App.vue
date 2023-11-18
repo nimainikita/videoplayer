16c5f0cd3374d4c87ef15b4923bb780034ca460b
nimainikita <122300972+nimainikita@users.noreply.github.com>
2023-11-18 15:58:01 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="container">
+  <video-player />
+</div>
+</template>
+
+<script>
+import VideoPlayer from './components/player/VideoPlayer.vue';
+export default {
+  components:{VideoPlayer}
+};
+</script>
